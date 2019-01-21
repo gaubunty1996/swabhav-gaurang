e1f36f7fa5b61e03977722faa33712ef19f69dcb
Gaurang <gaurangk27@gmail.com>
2019-01-21 23:37:57 +0530

    now displays perks applicable to each employee

--- Oops/employee-polymorphism/src/com/techlab/employeeTest/GenerateSalary.java
+++ Oops/employee-polymorphism/src/com/techlab/employeeTest/GenerateSalary.java
@@ -8,22 +8,32 @@ import com.techlabs.employee.Manager;
 public class GenerateSalary {
 	public static void printPaySlip(Employee employee) {
 		employee.salary();
-		System.out.println("Employee's salary : "+employee.getSalary());
-		System.out.println("Employee's house rentanl allowance : "+employee.getHouseRentalAllowance());
-		System.out.println("Employee's performance allowance : "+employee.getPerformanceAllowance());
-		System.out.println("Employee's travel allowance : "+employee.getTravelAllowance());
-		System.out.println("Employee's perks : "+employee.getPerks());
-		System.out.println("Employee's role : "+employee.getRole());
+		System.out.println("Employee's role : " + employee.getRole());
+		System.out.println("Employee's salary : " + employee.getSalary());
+		if(employee.getHouseRentalAllowance()>0) {
+			System.out.println("Employee's house rentanl allowance : " + employee.getHouseRentalAllowance());
+		}
+		if(employee.getPerformanceAllowance()>0) {
+			System.out.println("Employee's performance allowance : " + employee.getPerformanceAllowance());
+		}
+		if(employee.getTravelAllowance()>0) {
+			System.out.println("Employee's travel rentanl allowance : " + employee.getTravelAllowance());
+		}
+		if(employee.getPerks()>0) {
+			System.out.println("Employee's perks : " + employee.getPerks());
+		}
 		System.out.println("\n");
+		
+		
 	}
 
 	public static void main(String[] args) {
 		Manager manager = new Manager("Gaurang", 1, "Manager");
 		printPaySlip(manager);
-		
+
 		Accountant accountant = new Accountant("Jaaie", 2, "Accountant");
 		printPaySlip(accountant);
-		
+
 		Developer developer = new Developer("soham", 3, "Developer");
 		printPaySlip(developer);
 	}
