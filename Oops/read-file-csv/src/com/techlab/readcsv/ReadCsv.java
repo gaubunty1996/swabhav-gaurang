d8bcdb693adec9d1e37cf09fe6ad770a26f44bf3
Gaurang <gaurangk27@gmail.com>
2019-01-25 23:32:12 +0530

    csv with oops

--- Oops/read-file-csv/src/com/techlab/readcsv/ReadCsv.java
+++ Oops/read-file-csv/src/com/techlab/readcsv/ReadCsv.java
@@ -10,27 +10,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ReadCsv {
+	public static Map<String, String> hashmap = new HashMap<String, String>();
 
 	public static void main(String[] args) throws IOException {
-		
-		Map<String, String> hashmap = new HashMap<String, String>();
-		
 		String fileName = "C:\\Users\\gaurang\\Desktop\\read.csv";
+		csvFileReader(fileName);
+		System.out.println(hashmap);
+	}
+
+	public static void csvFileReader(String fileName) throws IOException {
+
 		File file = new File(fileName);
 		BufferedReader br = new BufferedReader(new FileReader(file));
-
 		String st;
 		while ((st = br.readLine()) != null) {
-			st=st.toLowerCase();
-			String[] names=st.split(",");
-			for(int i=0;i<names.length;i++) {
-				hashmap.put(names[0].trim(), names[1].trim());
-			}
+			st = st.toLowerCase();
+			String[] names = st.split(",");
+			addDataInHashMap(names);
 			System.out.println(Arrays.toString(names));
 		}
 		System.out.println("\n");
 		br.close();
-		System.out.println(hashmap);
+	}
+
+	public static void addDataInHashMap(String[] names) {
+		for (int i = 0; i < names.length; i++) {
+			hashmap.put(names[0].trim(), names[1].trim());
+		}
 	}
 
 }
